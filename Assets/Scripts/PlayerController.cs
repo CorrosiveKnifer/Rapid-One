622c27ea97f2d546853881ca82b8d9fcdb7fe8de
dk23k <55791230+GrizzBe@users.noreply.github.com>
2021-03-10 14:35:38 +1300

    Player prefab
    Created player prefab and introduced ground layer to allow for jumping. Player is currently represented by a grey cylinder. Currently only the PlayerMovement script is necessary.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float m_fMouseSensitivity = 100f;
+    public Transform m_PlayerBody;
+
+    float m_RotationX = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Don't use please, it's trash
+        float mouseX = Input.GetAxis("Mouse X") * m_fMouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * m_fMouseSensitivity * Time.deltaTime;
+
+        //m_RotationX -= mouseY;
+        //m_RotationX = Mathf.Clamp(m_RotationX, -90f, 90f);
+
+        //transform.localRotation = Quaternion.Euler(m_RotationX, 0f, 0f);
+        //m_PlayerBody.Rotate(Vector3.up * mouseX);
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public bool m_bIsChild = false;
+
+    public CharacterController m_CharController;
+    public float m_fSpeed = 12f; // Move speed
+    public float m_fGravity = -9.81f;
+
+    public Vector3 m_vVelocity;
+    public bool m_bGrounded;
+    float m_fJumpThrust = 5.0f;
+
+    public Transform m_GroundCheck;
+    public float m_GroundDistance = 0.4f;
+    public LayerMask m_GroundMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (m_bIsChild) // Child numbers
+        {
+            m_fSpeed = 3.0f;
+            m_fJumpThrust = 5.0f;
+        }
+        else // Adult numbers
+        {
+            m_fSpeed = 6.0f;
+            m_fJumpThrust = 8.0f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Ground check
+        if ((Physics.CheckSphere(m_GroundCheck.position, m_GroundDistance, m_GroundMask)))
+        {
+            m_bGrounded = true;
+            Debug.Log("Grounded");
+        }
+        else
+        {
+            m_bGrounded = false;
+            Debug.Log("Not Grounded");
+        }
+
+        // Snap to ground
+        if (m_bGrounded && m_vVelocity.y < 0)
+        {
+            m_vVelocity.y = -2f;
+        }
+        // Jump
+        if (m_bGrounded && Input.GetButtonDown("Jump"))
+        {
+            m_vVelocity.y = m_fJumpThrust;
+        }
+
+        // Movement inputs
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // Create vector from player's current orientation (meaning it will work with rotating camera)
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        // Apply gravity to velocity
+        m_vVelocity.y += m_fGravity * Time.deltaTime;
+
+        // Apply both movement inputs and velocity to the character controller
+        m_CharController.Move((move * m_fSpeed + m_vVelocity) * Time.deltaTime);
+    }
+}
