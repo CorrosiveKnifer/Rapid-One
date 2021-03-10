06ad8eccd1e9a3774bf21d706cbfe6ecfe39ab3d
Michael Jordan <michdan25@gmail.com>
2021-03-10 14:15:47 +1300

    Camera Transition
    Created a camera which is controlled on a NavMesh that switches between the adult and child.

---
+++ Assets/CameraAgent.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CameraAgent : MonoBehaviour
+{
+    public Transform childLocation;
+    public Transform parentLocation;
+
+    private NavMeshAgent agent;
+
+    public int state = 0;
+    public bool isShifting = false;
+    private Transform targetLocation;
+    private Transform shiftingTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        targetLocation = parentLocation;
+        state = 1;
+        shiftingTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isShifting)
+        {
+            agent.destination = targetLocation.position;
+            float dist = Vector3.Distance(transform.position, targetLocation.position);
+            transform.localScale =  LerpToTarget(targetLocation.localScale, shiftingTransform.localScale, dist, 2.5f);
+        }
+        else
+        {
+            transform.position = targetLocation.position;
+        }
+
+        if(isShifting && IsPositionEquals(transform.position, targetLocation.position))
+        {
+            isShifting = false;
+            state = (targetLocation.position.Equals(childLocation.position)) ? -1 : 1;
+        }
+    }
+
+    public void Shift()
+    {
+        isShifting = true;
+        shiftingTransform = transform;
+        if (IsPositionEquals(transform.position, childLocation.position))
+        {
+            targetLocation = parentLocation;
+        }
+        else
+        {
+            targetLocation = childLocation;
+        }
+    }
+
+    private bool IsPositionEquals(Vector3 pos1, Vector3 pos2, float threshold = 0.05f)
+    {
+        return (Vector3.SqrMagnitude(pos1 - pos2) < threshold);
+    }
+
+    private Vector3 LerpToTarget(Vector3 current, Vector3 target, float distance, float threshold = 1.5f)
+    {
+        float distRatio = Mathf.Clamp(distance / threshold, 0.0f, 1.0f);
+
+        Vector3 retValue = Vector3.Lerp(current, target, distRatio);
+
+        return retValue;
+    }
+}
---
+++ Assets/CameraController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    #region SingletonImplementation
+    public static CameraController instance { get; private set; }
+    
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = new CameraController();
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    #endregion
+
+    public Camera parentCamera;
+    public Camera childCamera;
+    public Camera ghostCamera;
+    public CameraAgent agent;
+
+    void Start()
+    {
+        ghostCamera.enabled = false;
+        childCamera.enabled = false;
+        parentCamera.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            parentCamera.enabled = false;
+            ghostCamera.enabled = true;
+            childCamera.enabled = false;
+            agent.Shift();
+        }
+
+        if(!agent.isShifting && !(childCamera.enabled || parentCamera.enabled))
+        {
+            switch (agent.state)
+            {
+                default:
+                case -1:
+                    parentCamera.enabled = false;
+                    ghostCamera.enabled = false;
+                    childCamera.enabled = true;
+                    break;
+                case 1:
+                    parentCamera.enabled = true;
+                    ghostCamera.enabled = false;
+                    childCamera.enabled = false;
+                    break;
+            }
+
+        }
+    }
+}
