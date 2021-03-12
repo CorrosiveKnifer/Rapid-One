ab44d9aedfbbd4c833ae42c087542dd7678617f2
RachaelColaco <65530256+RachaelColaco@users.noreply.github.com>
2021-03-12 17:54:38 +1300

    Inheritances

---
+++ Assets/Scripts/Inheritance/Animal.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+   // public Transform cam;
+    public string Name;
+
+    public virtual void MakeSound()
+    {
+        Debug.Log("No sound Detected");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //MakeSound();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            //list of object that get hit by the raycast
+            RaycastHit[] hits;
+
+            //to see the line of the raycast
+            Debug.DrawRay(transform.position, cam.forward * 5.0f, Color.blue, 2);
+            hits = Physics.RaycastAll(transform.position, cam.forward, 5.0f);
+
+            //checking each item
+            foreach (var hit in hits)
+            {
+                //if the object tag is key to detroy and admit to key
+                Animal other = hit.collider.gameObject.GetComponentInChildren<Animal>();
+                if (other != null)
+                {
+                    other.MakeSound();
+                    //break;
+                }
+            }
+
+
+        }
+        */
+    }
+}
---
+++ Assets/Scripts/InteractingItems.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractingItems : MonoBehaviour
+{
+    public Transform cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //i used F for now
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            //list of object that get hit by the raycast
+            RaycastHit[] hits;
+
+            //to see the line of the raycast
+            Debug.DrawRay(transform.position, cam.forward * 5.0f, Color.blue, 2);
+            hits = Physics.RaycastAll(transform.position, cam.forward, 5.0f);
+
+            //checking each item
+            foreach (var hit in hits)
+            {
+               
+
+                //chekcing and calling the functions to interact with that certain item
+                ///NOTE: do change the script of animals into items.
+                /*
+                Animal other = hit.collider.gameObject.GetComponentInChildren<Animal>();
+                if (other != null)
+                {
+                    other.MakeSound();
+                    break;
+                }
+                */
+                Interactable other = hit.collider.gameObject.GetComponentInChildren<Interactable>();
+                if (other != null)
+                {
+                    other.Activate();
+                    break;
+                }
+            }
+
+
+        }
+    }
+}
