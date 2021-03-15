a626b5b963c3f47126d68da794deb3742c0f2f8e
Michael Jordan <michdan25@gmail.com>
2021-03-15 16:43:30 +1300

    fixed shift bug

---
+++ Assets/DoorScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : Interactable
+{
+    public bool isLocked;
+    private bool isClosed = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Activate()
+    {
+        if(!isLocked)
+        {
+            isClosed = !isClosed;
+        }
+    }
+}
