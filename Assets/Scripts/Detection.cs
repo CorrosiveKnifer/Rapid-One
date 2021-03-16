77deba8698a48b78bd53cf37062b2ee606a72621
Michael Jordan <michdan25@gmail.com>
2021-03-16 14:09:28 +1300

    More housing stuff

--- Assets/Scripts/Detection.cs
+++ Assets/Scripts/Detection.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Detection : MonoBehaviour
 {
     public Camera camera;
+    public GameObject HUDObject;
+
+    private HUDScript HUD;
     public virtual void Detect()
     {
         Debug.Log("No sound Detected");
@@ -14,6 +17,7 @@ public class Detection : MonoBehaviour
     void Start()
     {
         //Detect();
+        HUD = HUDObject.GetComponent<HUDScript>();
     }
 
     // Update is called once per frame
@@ -28,7 +32,8 @@ public class Detection : MonoBehaviour
         Ray ray = camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0.0f));
         Debug.DrawRay(ray.origin, ray.direction * 5.0f, Color.red, 0.5f);
         hits = Physics.RaycastAll(ray.origin, ray.direction, 5.0f);
-
+        
+        bool hasHit = false;
         //checking each item
         foreach (var hit in hits)
         {
@@ -40,15 +45,23 @@ public class Detection : MonoBehaviour
             if (hit.collider.tag == "Liftable")
             {
                 //Debug.Log("Lifting");
-                gameObject.GetComponent<Liftable>().Detect();
+                //gameObject.GetComponent<Liftable>().Detect();
+                HUD.ShowHand();
+                hasHit = true;
             }
             ///may need to fix this on different tags
             Interactable other = hit.collider.gameObject.GetComponentInChildren<Interactable>();
             if (other != null)
             {
                 Debug.Log("interact");
+                hasHit = true;
                 break;
             }
+        }
+
+        if(!hasHit)
+        {
+            HUD.ShowCursor();
         }
     }
     
