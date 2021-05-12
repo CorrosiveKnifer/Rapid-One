b85d3272db4a3c3cfa8a6a2b336861e07bcb591b
RachaelColaco <65530256+RachaelColaco@users.noreply.github.com>
2021-05-12 17:11:46 +1200

    changing material, map and detection scripts

---
+++ Assets/Scripts/DetectArea.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DetectArea : MonoBehaviour
+{
+
+    public Image currentPlayerPic;
+    public GameObject HUB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //currentPlayerPic.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Player")
+        {
+            HUB.GetComponent<MapOfHouse>().playerPic.enabled = false;
+            HUB.GetComponent<MapOfHouse>().playerPic = currentPlayerPic;
+            HUB.GetComponent<MapOfHouse>().playerPic.enabled = true;
+            //currentPlayerPic.transform.position;
+        }
+    }
+}
---
+++ Assets/Scripts/MapOfHouse.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class MapOfHouse : MonoBehaviour
+{
+    public GameObject map;
+    public Image playerPic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        map.SetActive(false);
+        playerPic.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("i"))
+        {
+            map.SetActive(!map.activeSelf);
+
+        }
+        if (Input.GetKeyDown("r"))
+        {
+            map.SetActive(false);
+        }
+    }
+
+}
