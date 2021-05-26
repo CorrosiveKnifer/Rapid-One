03c03c8675922a17e86aa9554f42fad96f0b9ad4
FetusChan <62039548+FetusChan@users.noreply.github.com>
2021-05-26 23:22:03 +1200

    why wont pause menu lock movement

--- Assets/Scripts/PauseMenuScreen.cs
+++ Assets/Scripts/PauseMenuScreen.cs
@@ -5,32 +5,52 @@ using UnityEngine;
 
 public class PauseMenuScreen : MonoBehaviour
 {
-    public GameObject pMenu;
+    public static bool GameIsPaused = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        pMenu.SetActive(false);
-    }
+    public GameObject pMenu;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("l"))
-        {
-            pMenu.SetActive(!pMenu.activeSelf);        
-        }
-
-        if (pMenu.activeSelf)
-        {
-            PlayerController.instance.SetCameraFreeze(true); 
-            Time.timeScale = 0;
-              
-        }
-        else
-        {
-            PlayerController.instance.SetCameraFreeze(false); 
-            Time.timeScale = 1;
+        if (Input.GetKeyDown("escape"))
+        { 
+            if (GameIsPaused)
+            {
+                Resume();
+            }    
+            else
+            {
+                Pause();
+            }
         }
     }
+
+    public void Resume()
+    {
+        pMenu.SetActive(false);
+        //PlayerController.instance.SetCameraFreeze(false); 
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pMenu.SetActive(true);
+        //PlayerController.instance.SetCameraFreeze(true); 
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.Confined;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("ass");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("ass");
+    }
 }
+       
