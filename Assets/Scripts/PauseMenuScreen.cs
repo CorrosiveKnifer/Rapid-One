0bec571df4a1f001a2fd58547f93f4d850c26fdf
peter99102 <quocanh1999wow@gmail.com>
2021-05-27 03:57:30 +1200

    code added - Petre
    added code variations in pause menu scripts to freeze the game when activated under comment mode

--- Assets/Scripts/PauseMenuScreen.cs
+++ Assets/Scripts/PauseMenuScreen.cs
@@ -6,24 +6,31 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenuScreen : MonoBehaviour
 {
-    public static bool GameIsPaused = false;
+    //public static bool GameIsPaused = false;
+    public bool GameIsPaused;
 
     public GameObject pMenu;
 
-    // Update is called once per frame
-    void Update()
+
+    /*void Update()
     {
-        if (Input.GetKeyDown("escape"))
-        { 
-            if (GameIsPaused)
-            {
-                Resume();
-            }    
-            else
-            {
-                Pause();
-            }
+        if (GameIsPaused)
+        {
+            pMenu.SetActive(true);
+            Time.timeScale = 0f;
         }
+        else
+        {
+            pMenu.SetActive(false);
+            Time.timeScale = 1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GameIsPaused = !GameIsPaused;
+        }
+
+        
     }
 
     public void Resume()
@@ -51,7 +58,57 @@ public class PauseMenuScreen : MonoBehaviour
 
     public void QuitGame()
     {
+       
         SceneManager.LoadScene("MenuScreen");
+    }*/
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            { 
+                if (GameIsPaused)
+                {
+                    Resume();
+                
+
+            }    
+                else
+                {
+                    Pause();
+               
+                    
+                }
+            }
+        }
+
+        public void Resume()
+        {
+            pMenu.SetActive(false);
+            //PlayerController.instance.SetCameraFreeze(false); 
+            Time.timeScale = 1f;
+            Cursor.lockState = CursorLockMode.Locked;
+            GameIsPaused = false;
+        }
+
+        public void Pause()
+        {
+            pMenu.SetActive(true);
+            //PlayerController.instance.SetCameraFreeze(true); 
+            Time.timeScale = 0f;
+            Cursor.lockState = CursorLockMode.Confined;
+            GameIsPaused = true;
+        }
+
+        public void Restart()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        public void QuitGame()
+        {
+            SceneManager.LoadScene("MenuScreen");
+        }
     }
-}
        
