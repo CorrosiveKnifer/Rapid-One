82dbe11f14940c86f688427277cb475220db80cf
William <55791230+GrizzBe@users.noreply.github.com>
2021-05-29 16:26:44 +1200

    Objectivemanager and objective

---
+++ Assets/ObjectiveManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveManager : MonoBehaviour
+{
+    public Objective[] Objectives;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool allComplete = true;
+        foreach (var obj in Objectives)
+        {
+            if (!obj.isComplete)
+                allComplete = false;
+        }
+
+        if (allComplete)
+        {
+            // show bed objective
+        }
+        else
+        {
+            // show other objectives
+        }
+    }
+}
---
+++ Assets/Scripts/Objective.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objective : Interactable
+{
+    public bool isComplete = false;
+    public string objectiveName = "Objective";
+    private string goalText;
+    public bool hasAnimation = false;
+    public MeshRenderer togglableMesh;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        goalText = "[ ] " + objectiveName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public override void Activate(Interactor other)
+    {
+        if (!isComplete)
+        {
+            // Change bool
+            isComplete = true;
+
+            // Play sound
+            if (GetComponent<AudioAgent>())
+            {
+                AudioAgent agent = GetComponent<AudioAgent>();
+                if (agent.AudioClips.Length != 0)
+                    agent.PlaySoundEffect(agent.AudioClips[0].name);
+            }
+
+            // Change model / mat
+            if (togglableMesh != null)
+            {
+                togglableMesh.enabled = false;
+            }
+            if (animator != null)
+            {
+                animator.SetTrigger("Start");
+            }
+
+            // Debug log
+            Debug.Log("The objective " + objectiveName + " has been completed");
+            goalText = "[X] " + objectiveName;
+        }
+    }
+}
